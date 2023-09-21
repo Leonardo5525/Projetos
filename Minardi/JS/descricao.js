3c32ef2ddcfb3534f7361dc03eb3d2fe20003eb7
Leonardo Nakamura <leonardonaka75@gmail.com>
2023-09-20 22:17:00 -0300

    fazer o merge

--- Minardi/JS/descricao.js
+++ Minardi/JS/descricao.js
@@ -4,3 +4,14 @@ value.textContent = input.value;
 input.addEventListener("input", (event) => {
   value.textContent = event.target.value;
 });
+
+const heart = document.getElementById("like")
+heart.addEventListener("click", () => {
+    if (heart.className === 'bx bx-heart') {
+      heart.classList.remove("bx-heart")
+      heart.classList.add("bxs-heart")
+    }else {
+      heart.classList.add("bx-heart")
+      heart.classList.remove("bxs-heart")
+    }
+})
